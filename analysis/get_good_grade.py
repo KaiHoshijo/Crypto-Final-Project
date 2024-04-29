f13a9c8a932875d1d741fb01f3090d78fa6ad443
TheNathanSpace <nathan@thenathan.space>
2024-04-29 00:02:54 -0600

    uh

--- analysis/get_good_grade.py
+++ analysis/get_good_grade.py
@@ -1,9 +1,20 @@
+from analysis import get_traces, get_analysis
+
+
 def do_stuff():
     # 1. Read traces t
+    power_traces = get_traces.get_traces()
+
     # 2. Calculate intermediate values for all d (plaintext block) and k (subkey) combinations
+    "do stuff"
+
     # 3. Build power model h by calculating Hamming Weight for each intermediate value
-    # 4. Calculate correlation between power model h and traces t
-    # 5. Get subkey from the highest correlation
-    # 6. Somehow get from a subkey to the entire key?
+    power_estimates = "do stuff"
+
+    # 4. Calculate correlation between power model h and traces t. Get subkey from the highest correlation.
+    subkey = get_analysis.pick_subkey(power_estimates, power_traces)
+
+    # 5. Somehow get from a subkey to the entire key?
+    "what"
 
     pass
--- analysis/get_traces.py
+++ analysis/get_traces.py
@@ -2,18 +2,19 @@ from numpy import genfromtxt
 import numpy as np
 import os
 
+
 def get_traces():
     try:
         # Load the traces if they already exist
         traces = np.load('analysis/traces.npz')
         traces = traces[traces.files[0]]
     except FileNotFoundError:
-        traces = np.empty(shape = (2000,2000,2)) # Array of all the traces
+        traces = np.empty(shape=(2000, 2000, 2))  # Array of all the traces
         i = 0
         # If the traces don't already exist, load them
         for wavename in os.listdir('waveforms'):
             # First two rows are name columns
-            data = genfromtxt(f'waveforms/{wavename}', delimiter = ',')[2:]
+            data = genfromtxt(f'waveforms/{wavename}', delimiter=',')[2:]
             traces[i] = data
             i += 1
         # Save the traces so that this expensive operation doesn't occur often
