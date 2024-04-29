87ab92292cf487e03b7b7aee381aa1760ad3e957
TheNathanSpace <nathan@thenathan.space>
2024-04-28 23:59:14 -0600

    uh?

---
+++ analysis/get_good_grade.py
@@ -0,0 +1,9 @@
+def do_stuff():
+    # 1. Read traces t
+    # 2. Calculate intermediate values for all d (plaintext block) and k (subkey) combinations
+    # 3. Build power model h by calculating Hamming Weight for each intermediate value
+    # 4. Calculate correlation between power model h and traces t
+    # 5. Get subkey from the highest correlation
+    # 6. Somehow get from a subkey to the entire key?
+
+    pass
