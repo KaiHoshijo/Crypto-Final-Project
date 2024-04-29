dab7aa7673efb12ebe11b2970eceb24d19ee2054
Kai Miller <kaihoshijo003@gmail.com>
2024-04-28 21:06:57 -0600

    The code for the arduino as well as the power traces

---
+++ gen_aes.ino
@@ -0,0 +1,64 @@
+#include "Crypto.h"
+#include "RNG.h"
+#include "TransistorNoiseSource.h"
+#include "CBC.h"
+#include "AES.h"
+
+// Number of times to generate and encrypt plaintext
+#define COUNT 400
+
+TransistorNoiseSource noise(A1);
+CBC<AESTiny128> cbc;
+byte key[16] = {0xA2,0xD6,0xBF,0x5A,0x35,0xA1,0x38,0x66,0x6f,0x24,0xC0,0x9C,0x50,0xAE,0xFC,0x43};
+byte iv[16] = {0xD7,0xB3,0x00,0x8E,0xE1,0xFD,0x8A,0x05,0xC7,0x5D,0x30,0x4D,0x36,0xA6,0x45,0xBB};
+
+void setup() {
+  // Creating the RNG class
+  RNG.begin("Crypto Final Project");
+  RNG.addNoiseSource(noise);
+  // Generating the key
+  // RNG.rand(key, sizeof(key));
+  // RNG.rand(iv, sizeof(iv));
+  cbc.setKey(key, sizeof(key));
+  cbc.setIV(iv, sizeof(iv));
+  // Setting A2 to digital to trigger in AES128 right after the subbytes
+  pinMode(A2, OUTPUT);
+  Serial.begin(9600);
+  // Output the key
+  Serial.print("Key ");
+  for (uint8_t i = 0; i < 16; i++) {
+    Serial.print(key[i], HEX);
+  }
+  Serial.println();
+  // Output the iv
+  Serial.print("IV ");
+  for (uint8_t i = 0; i < 16; i++) {
+    Serial.print(iv[i], HEX);
+  }
+  Serial.print('\n');
+}
+
+void loop() {
+  RNG.loop();
+  byte input[16];
+  byte output[16];
+  for (uint16_t i = 0; i < COUNT; i++) {
+    // Wait until the oscope code sends the command to run
+    while (!Serial.available());
+    String incoming = Serial.readString();
+    // Generate the plaintext
+    RNG.rand(input, sizeof(input));
+    // Output the newly generated plaintext
+    Serial.print("Input ");
+    for (uint8_t j = 0; j < 16; j++) {
+      Serial.print(input[j], HEX);
+    }
+    Serial.println();
+    // Encrypting the plaintext using aes-cbc
+    cbc.encrypt(output, input, sizeof(input));
+    // Set the pin set in the aes-cbc code to low
+    digitalWrite(A2, LOW);
+  }
+  // Idle once the 255 waves are retrieved
+  for(;;);
+}
---
+++ get_waves.py
@@ -0,0 +1,136 @@
+import pyvisa
+import time
+import numpy as np
+import serial
+import os
+
+'''
+The configurations of the Keysight InfiniiVision MSOX3012T Oscilloscope is
+as follows:
+
+Channel 1: 5 (V/div)
+Channel 2: 50m (V/div)
+Period (s): 1 u
+Trigger Mode: Edge
+Trigger Source: 1
+Trigger Type: Rising
+'''
+
+INTERVAL = 0.5
+TIMEOUT = 100000
+WAVEFORM = 2000
+PORT = 'COM7'
+
+rm = pyvisa.ResourceManager()
+# Open the oscilloscope resource
+instr = rm.open_resource(rm.list_resources()[0])
+instr.timeout = TIMEOUT 
+ser = serial.Serial(port = PORT, baudrate = 9600, parity=serial.PARITY_ODD,
+                    stopbits=serial.STOPBITS_TWO, bytesize=serial.SEVENBITS)
+
+IDN = instr.query('*IDN?')
+print(IDN.strip())
+
+def init():
+    # Reset the board
+    instr.query('*RST; *OPC?')
+    # Configure the first channel
+    instr.write(':CHANnel1:DISPlay ON')
+    instr.write(':CHANnel2:DISPlay ON')
+    instr.write(':CHANnel1:SCALe 5')
+    instr.write(':CHANnel1:OFFSet -10')
+    # Configure the second channel
+    instr.write(':CHANnel2:SCALe .05')
+    instr.write(':CHANnel2:OFFSet .05')
+    # Configure horizontal scale and offset
+    instr.write(':TIMebase:SCALe 0.000001')
+    instr.write(':TIMebase:POSition 0')
+    # Set the trigger mode
+    instr.write(':TRIGger:MODE EDGE')
+    # Set the source for the trigger
+    instr.write(':TRIGger:EDGE SOURce CHANnel1')
+    # Set the threshold for the trigger
+    instr.write(':TRIGger:EDGE:LEVel 2.5V')
+    # Set the waveform format
+    instr.write(':SAVE:WAVeform:FORMat CSV')
+    # instr.write(':SAVE:WAVeform:LENGth 1000')
+
+def read_serial():
+    out = ''
+    while ser.inWaiting() > 0:
+        a = ser.read(1).decode()
+        if a == '\r':
+            print("Testing", a)
+        else:
+            out += a
+    return out
+
+
+# Initialize the oscope
+init()
+print(f'Capturing this number of waveforms: {WAVEFORM}')
+# Get the latest file name from waveforms
+i = 0
+with open("waveforms.txt", "r") as f:
+    lines = f.readlines()
+    if len(lines) != 0:
+        for i in range(len(lines) - 1, -1, -1):
+            line = lines[i]
+            if '.csv' in line:
+                i = int(line[1:].split('.csv')[0]) + 1
+                break
+print(i)
+# Get the key and the iv from the arduino
+output = read_serial()
+if i != 0: # Only get key and iv if values haven't been retrieved already
+    output = ''
+print(output)
+instr.write('*ESE 255')
+# Start the serial by writing to it
+try:
+    while i < WAVEFORM:
+        # Send the command to run
+        ser.write(b'1')
+        # Turn the display for channel 1 on after turning it off for the image
+        instr.write(':CHANnel1:DISPlay ON')
+        # Wait for one trigger
+        instr.write('*CLS;:SINGle')
+        # Wait until the encryption happens before running next code block
+        triggered = 0
+        while triggered == 0:  # Poll the scope until it returns a 1
+            triggered = int(instr.query(':TER?'))
+            time.sleep(INTERVAL)  # Pause to prevent excessive queries
+        # Name the file to be saved
+        filename = f'W{i}'
+        i += 1
+        # Turn off channel 1 so its data doesn't conflict the power analysis
+        instr.write(':CHANnel1:DISPlay OFF')
+        # Set the file name and the file type
+        instr.write(f':SAVE:FILename "{filename}"')
+        instr.write(':SAVE:WAVeform:FORMat CSV')
+        # Run continuously if there is an error so that I can fix it
+        num = ''
+        while num != '0':
+            # Try to write to usb first
+            instr.write(f':SAVE:WAVeform "/usb/{filename}.csv"')
+            res = instr.query('*OPC?')
+            num,msg = instr.query(':SYSTem:ERRor?').split(',')
+            # If there is an error, try writing to usb2
+            if '257' in num:
+                instr.write(f':SAVE:WAVeform "/usb2/{filename}.csv"')
+                res = instr.query('*OPC?')
+                num,msg = instr.query(':SYSTem:ERRor?').split(',')
+            # Print the error output
+            num = num[1:] # should be zero if nothing went wrong
+            if num != '0':
+                print(msg.strip())
+            print(instr.query('*ESR?').strip())
+            time.sleep(1) # Wait for one second to not overwhelm the oscope
+        # Get the plaintext and the first round encryption
+        output += f'{filename}.csv\n'
+        print(filename)
+        output += read_serial()
+    print(f'Captured this number of of waveforms: {WAVEFORM}')
+finally:
+    with open('waveforms.txt', 'a') as f:
+        f.write(output)
